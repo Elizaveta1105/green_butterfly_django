229b124d0d3dcf13806ea5b09e83a64e422dceb8
Yelyzaveta <elizaveta@short.io>
2025-04-18 10:38:44 +0300

    installed dotenv

--- app_finance/settings.py
+++ app_finance/settings.py
@@ -1,9 +1,9 @@
 from os import getenv
 from pathlib import Path
-import cloudinary
-import cloudinary.uploader
 import cloudinary.api
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
